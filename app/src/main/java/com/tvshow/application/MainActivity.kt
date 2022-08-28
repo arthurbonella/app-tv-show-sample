30548d5036a08f87dddafb56cdd1cab929c7289d
Bonella <arthur_bonella@hotmail.com>
2022-08-28 08:29:17 -0300

    Project config

---
+++ app/src/main/java/com/tvshow/application/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tvshow.application
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tvshow.tv.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
